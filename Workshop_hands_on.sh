bb698715f2c2303666d3bb74b03bc8f9ca2e7169
cespa1 <cesar.ppb00@gmail.com>
2026-04-07 18:18:29 +0200

    codigo actualizado

--- Workshop_hands_on.sh
+++ Workshop_hands_on.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-mkdir -p peak_calling
-mkdir -p fastqc
+
 ## Reducción de tamaño de los fastq para poder hacer los analisis durante el workshop 
 #zcat ENCFF047YJC.fastq.gz | seqtk sample -s100 - 500000 > R1_subset_A549.fastq
 #zcat ENCFF048FVL.fastq.gz | seqtk sample -s100 - 500000 > R2_subset_A549.fastq
@@ -38,7 +37,7 @@ multiqc .
 ## Alineamiento de las muestras usando bowtie2
 for muestra in $(ls | grep R1_subset_trim_ | grep ".fastq\b" | cut -d '_' -f4 | cut -d '.' -f1 | uniq )
 do 
-bowtie2 -x ../fastq/human_index -1 "R1_subset_trim"*$muestra*".fastq.gz" -2 "R2_subset_trim"*$muestra*".fastq.gz" -S $muestra".sam" -X 500 -p 23 2> "bowtie_log_"$muestra".txt"
+bowtie2 -x ../fastq/human_index -1 "R1_subset_trim"*$muestra*".fastq.gz" -2 "R2_subset_trim"*$muestra*".fastq.gz" -S $muestra".sam" -X 500 -p 23 --very-sensitive 2> "bowtie_log_"$muestra".txt"
 
 ##Ordenamiento del SAM
 samtools sort $muestra".sam" -o $muestra"_sort.sam"
@@ -51,7 +50,7 @@ samtools view -bhS $muestra"_sort_dedup.sam" > $muestra"_sort_dedup.bam"
 samtools index $muestra"_sort_dedup.bam" $muestra"_sort_dedup.bai"
 
 ## Visualización de la muestras en IGV
-igvtools count -z 5 -w 25 -e 250 $muestra"_sort_dedup.bam" $muestra"_dedup_sorted.tdf" Homo_sapiens.GRCh38.dna.primary_assembly.fa
+igvtools count -z 5 -w 25 -e 250 $muestra"_sort_dedup.bam" $muestra"_dedup_sorted.tdf" ../Homo_sapiens.GRCh38.dna.primary_assembly.fa
 
 ##Peak calling 
 macs2 callpeak -f BAMPE -g hs --keep-dup all --cutoff-analysis -n $muestra \
@@ -75,3 +74,28 @@ for muestra in $(ls | grep R1_subset_trim_ | grep ".fastq\b" | cut -d '_' -f4 | 
 do 
 htseq-count --format=bam --type=exon --idattr=gene_name $muestra"_sort_dedup.bam" peak_calling/merge_final.gtf  > $muestra"_htseq_counts"
 done
+
+###Analisis downstream Heatmap de accesibilidad con deeptools
+#for muestra in $(ls | grep R1_subset_trim_ | grep ".fastq\b" | cut -d '_' -f4 | cut -d '.' -f1 | uniq )
+#    do
+#        bamCoverage -p 23 -b $muestra"_sort_dedup.bam" -o bigwig/$muestra".bw" 
+#    done
+
+#computeMatrix reference-point --referencePoint center -p 23 -S bigwig/A549.bw bigwig/HepG2.bw bigwig/K562.bw -R peak_calling/merge_final_all.bed -b 3000 -a 3000 -bs=1 -p=max --outFileName Hands_on_matrix.matrix.gz
+
+#plotHeatmap -m Hands_on_matrix.matrix.gz --outFileName Hands_on_heatmap.pdf --yMax 10 --colorMap=Purples --zMax 10
+
+#plotHeatmap -m Hands_on_matrix.matrix.gz  -out Hands_on_heatmap_kmeans4.pdf --kmeans 4 --outFileSortedRegions ac_kmeans10.bed --zMin 0 --zMax 10 --yMin 0 --yMax 10
+
+##Analisis downstream HOMER y predicción de factores de transcripción
+
+#bedtools getfasta -fi /home/scclab/Atacseq/Homo_sapiens.GRCh38.dna.primary_assembly.fa -bed peak_calling/merge_final_all.bed > HOMER/merge_final_all.fa
+
+#findMotifs.pl HOMER/merge_final_all.fa human HOMER/ -fasta ~/Atacseq/scrambled.fasta
+
+###Analisis downstream TOBIAS y footprinting
+
+#TOBIAS ATACorrect --bam $muestra".bam" --genome Homo_sapiens.GRCh38.dna.primary_assembly.fa --peaks peak_calling/merge_final_all.bed --blacklist ../hg38-blacklist.v2.bed --outdir TOBIAS/$muestra --cores 22 
+    
+#TOBIAS FootprintScores --signal TOBIAS/$muestra/$muestra*"_corrected.bw" --regions peak_calling/merge_final_A431.bed \
+#--output TOBIAS/$muestra/$muestra"_footprints.bw" --cores 22
